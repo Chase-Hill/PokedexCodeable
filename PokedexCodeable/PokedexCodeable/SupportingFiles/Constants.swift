743edb699acecd088b57ef360d51ece1b9342523
Chase <chaseaohill@gmail.com>
2023-02-28 12:16:34 -0500

    Completed network controller and made constants folder

---
+++ PokedexCodeable/PokedexCodeable/SupportingFiles/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  PokedexCodeable
+//
+//  Created by Chase on 2/28/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Pokemon {
+        static let pokedexBaseURL = "https://pokeapi.co/api/v2/pokemon"
+        static let 
+    }
+}
